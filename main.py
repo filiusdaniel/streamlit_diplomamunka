651f91a48b798b21a8592f20f8321fdc990c2e12
filiu <filius.daniel@gmail.com>
2023-04-11 21:11:03 +0200

    Commit proba 2

--- main.py
+++ main.py
@@ -188,8 +188,7 @@ if selected=='Predictor':
 
             ).update_layout(xaxis_title="Mileage", yaxis_title="Price")
 
-            fig_car_prices.update_traces(mode="markers+lines", hovertemplate=None)
-            fig_car_prices.update_layout(hovermode="x")
+
             #st.pyplot(fig)
             st.plotly_chart(fig_car_prices, use_container_width=True)
 
